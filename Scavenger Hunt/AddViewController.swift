44d0e934b5cd7d410b8a71ac763403314f9b56dd
Christina Karolewicz <racketgirl93@bellsouth.net>
2016-02-06 14:25:41 -0500

    uploading file

---
+++ Scavenger Hunt/AddViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Christina Karolewicz on 2/4/16.
+//  Copyright © 2016 Christina Karolewicz. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    @IBAction func cancel(sender: AnyObject) {
+    
+      dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBOutlet weak var textfile: UITextField!
+    var newItem : ScavengerHuntItem?
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "DoneItem" {
+            if let name = textfile.text {
+                if !name.isEmpty {
+                    newItem = ScavengerHuntItem(name: name)
+                }
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
